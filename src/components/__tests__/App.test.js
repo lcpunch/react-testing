4e70e48c837e6ca2c24bcde6dfad518af0f84948
lcpunch <lcpunchirolli@gmail.com>
2018-07-16 21:07:47 -0400

    Adding beforeEach to test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,12 +4,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let component;
+
+beforeEach(() => {
+  component = shallow(<App />);
+});
+
 it('shows a comment box', () => {
-  const component = shallow(<App />);
   expect(component.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-  const component = shallow(<App />);
   expect(component.find(CommentList).length).toEqual(1);
 });
