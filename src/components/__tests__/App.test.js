d8873366f1ce8f4d5a72db04a9eba37164e0b1bb
lcpunch <lcpunchirolli@gmail.com>
2018-07-16 20:11:29 -0400

    Testing for CommentList component to be in App

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
-<<<<<<< HEAD
   const component = shallow(<App />);
   expect(component.find(CommentBox).length).toEqual(1);
-=======
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Comment Box');
+});
 
-  ReactDOM.unmountComponentAtNode(div);
->>>>>>> 6093085debc83549a492f3ec38c0afb0a6ff8ded
+it('show a comment list', () => {
+  const component = shallow(<App />);
+  expect(component.find(CommentList).length).toEqual(1);
 });
