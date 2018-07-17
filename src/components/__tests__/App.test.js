2fa1b0188b902bdd99bbb63bbd57d0d09567ab27
lcpunch <lcpunchirolli@gmail.com>
2018-07-16 20:19:21 -0400

    Adding absolute path to .env

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const component = shallow(<App />);
   expect(component.find(CommentBox).length).toEqual(1);
 });
 
-it('show a comment list', () => {
+it('shows a comment list', () => {
   const component = shallow(<App />);
   expect(component.find(CommentList).length).toEqual(1);
 });
