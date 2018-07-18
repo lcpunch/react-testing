10cca67564db50fa1d2a63b6d321706685063f90
lcpunch <lcpunchirolli@gmail.com>
2018-07-17 22:51:50 -0400

    Added beforeEach and afterEach with unmount to work with fulldom

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
+let component;
+
+beforeEach(() => {
+  component = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
 it('has a text area and a button', () => {
-
-  const component = mount(<CommentBox />);
-
   expect(component.find('textarea').length).toEqual(1);
   expect(component.find('button').length).toEqual(1);
 });
