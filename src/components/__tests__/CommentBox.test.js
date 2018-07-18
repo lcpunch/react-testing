7019b20b97f90bd86f96e6cea9dcabc59162638f
lcpunch <lcpunchirolli@gmail.com>
2018-07-18 10:08:18 -0400

    Using simulate from enzyme to test value change in textarea

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,9 @@ it('has a text area and a button', () => {
   expect(component.find('textarea').length).toEqual(1);
   expect(component.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  component.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+});
