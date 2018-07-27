2d6f2cd85c67eeef0545f574e635c562fb53cd60
lcpunch <lcpunchirolli@gmail.com>
2018-07-27 14:24:59 -0400

    Using cheerio queries to search values in html

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -23,6 +23,12 @@ it('creates one LI per comment', () => {
   expect(component.find('li').length).toEqual(2);
 });
 
+it('shows the text from each comment', () => {
+
+  expect(component.render().text()).toContain('Comment 1');
+  expect(component.render().text()).toContain('Comment 2');
+});
+
 
 afterEach(() => {
   component.unmount();
