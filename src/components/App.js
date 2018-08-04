5a6679bb553d24586f91f48ede0cd6793613c5a3
lcpunch <lcpunchirolli@gmail.com>
2018-08-03 22:53:17 -0400

    Adding react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CommentList from 'components/CommentList';
 import CommentBox from 'components/CommentBox';
 
 export default () => {
   return (
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path="/post" component={CommentBox} />
+      <Route path="/" exact component={CommentList} />
     </div>
-);
+  );
 };
