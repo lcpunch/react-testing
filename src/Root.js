c5d17f498c8e87662a263606433ad1f4bd160f05
lcpunch <lcpunchirolli@gmail.com>
2018-09-08 14:09:27 -0400

    Finishing middleware stateValidator

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
@@ -9,7 +10,7 @@ export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
 
   return(
